f37914bd320c99eb530e46a897d80f0e1e3a5da5
jonathan <programadorjonathan@gmail.com>
2026-09-24 22:18:39 -0400

    update path

--- display.php
+++ display.php
@@ -1,6 +1,6 @@
 <?php
 
-include 'vendor/Issues/Display/Routes/Start.php';
+include 'vendor/issuesphp/framework/src/Issues/Display/Routes/Start.php';
 
 $routeNameController  = $_GET['controller'] ?? '';
 $routeNameMethod  = $_GET['method'] ?? '';
